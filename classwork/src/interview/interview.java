a50a657865f67c03837dcde57298d0c2e3117c1d
Paul Thanksgiving <pthanksdev@gmail.com>
2026-04-21 19:09:47 -0600

    feat: initialize multiple project structures and add UPay application resources and controllers

---
+++ classwork/src/interview/interview.java
@@ -0,0 +1,19 @@
+package interview;
+
+public class Interview {
+    int testA;
+    int testB;
+    int testC;
+    int average;
+
+
+            public int average () {
+            return ((testA + testB + testC) / 3);
+        }
+        void Average ( int x, int y, int z){
+            testA = x;
+            testB = y;
+            testC = z;
+        }
+
+}
