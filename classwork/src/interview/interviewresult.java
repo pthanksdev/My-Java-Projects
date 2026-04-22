0c1bfb12355e864d63f6217252973791112e31c5
Paul Thanksgiving <pthanksdev@gmail.com>
2026-04-21 19:09:47 -0600

    feat: initialize multiple project structures and add UPay application resources and controllers

---
+++ classwork/src/interview/interviewresult.java
@@ -0,0 +1,10 @@
+package interview;
+
+//public class interviewresult {
+  //      public static void main(String [] args){
+    //        interview Addobj = new interview();
+      //      System.out.println("candidate is eligible for second test" );
+     //   }
+   // }
+
+
