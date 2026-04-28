1402ef93f1f72fc76d0a44d5a537c4db392f372b
Paul Thanksgiving <pthanksdev@gmail.com>
2026-04-27 23:43:02 -0600

    feat: implement UPay application modules, controllers, and UI resources alongside various Java learning projects

---
+++ java test/src/maths.java
@@ -0,0 +1,44 @@
+public class maths {
+    int Num1;
+    int Num2;
+    int x;
+    int y;
+    int Num3;
+
+    public int dividetwonumbers() {
+        return (Num1 / Num2);
+    }
+
+    public int dividetwonumber() {
+        return (Num1 % Num3);
+    }
+
+    public int plus() {
+        return (x += 3);
+    }
+
+    public boolean loop() {
+        if (x > 10) {
+            return ( true);
+        } else {
+            return (false);
+        }
+    }
+
+
+
+
+    maths() {//default method
+        Num1 = 5;
+        Num2 = 2;
+        x = 5;
+        Num3 = 3;
+    }
+
+   // public int length() {
+     //   int[] arr = {1, 2, 3, 4, 5};
+       // for (int i = 0; i < arr.length; i++) {
+       // }
+
+
+}
---
+++ java test/src/result.java
@@ -0,0 +1,12 @@
+public class result {
+        public static void main(String [] args){
+            maths Addobj = new maths();
+            System.out.println("1a) the result of 5/2 is " + Addobj.dividetwonumbers());
+            System.out.println("2b) datatype of int x = 5 is integer");
+            System.out.println("3b) the result of 5%3 is " + Addobj.dividetwonumber());
+            System.out.println("4b) result of int x = 5;x += 3 is " + Addobj.plus());
+            System.out.println("5b) x is less than or equal to 10");
+
+        }
+    }
+
